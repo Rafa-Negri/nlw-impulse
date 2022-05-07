380965e55a81393a9d648aa32048fe35f93dafdb
rafa-negri <rafael.negri01@gmail.com>
2022-05-07 18:31:05 -0300

    FrontEnd

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Widget } from "./components/Widget";
+
+export function App(){
+  return <Widget />
+}
+
+  // Componentes são função que retornam Html #JSX ou TSX
+  //Primeira letra maiuscula e o nome da função igual ao chamado
+
+// Propriedade podendo ser como atributps do HTML Exemplo utilizando Butto text="Enviar" mostrando a variavel 
+
